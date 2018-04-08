0df023d6accb073f0dd7def881d1e58eddb89214
gigi.sayfan <gigi.sayfan@helix.com>
2018-04-07 17:36:16 -0700

    Support restarting game after victory

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const io = require('socket.io')()
 
-let board = Array(6).fill(0).map(x => Array(8).fill('white'))
-
+let board = null
 const players = {'red': null, 'yellow': null}
-
 let player = 'red'
-let gameOver = false
+
+
+function reset() {
+  board = Array(6).fill(0).map(x => Array(8).fill('white'))
+  players['red'] = null
+  players['yellow'] = null
+  player = 'red'
+}
 
 function checkVictory(i, j) {
   const c = board[i][j]
@@ -142,20 +147,19 @@ io.on('connection', function (socket) {
   socket.on('click', function (column) {
     // Ignore players clicking when it's not their turn
     if (players[player] !== socket) {
-      return
-    }
-    // Ignore clicks when game is over
-    if (gameOver) {
+      console.log('click from wrong player: ' + player === 'red' ? 'yellow' : 'red')
       return
     }
 
     // Ignore clicks on full columns
     if (board[0][column] !== 'white') {
+      console.log('click on full column: ' + column)
       return
     }
 
     // Ignore clicks before both players are connected
     if ((players['red'] == null) || (players['yellow'] == null)) {
+      console.log('click before all players are connected')
       return
     }
 
@@ -172,8 +176,11 @@ io.on('connection', function (socket) {
 
     // Check victory (only current player can win)
     if (checkVictory(row, column)) {
-      gameOver = true
       io.emit('victory', player)
+      // Disconnect players
+      players['red'].disconnect()
+      players['yellow'].disconnect()
+      reset()
       return
     }
 
@@ -183,8 +190,7 @@ io.on('connection', function (socket) {
   })
 })
 
-
+reset()
 const port = 1337
-
 io.listen(port)
 console.log('Listening on port ' + port + '...')
